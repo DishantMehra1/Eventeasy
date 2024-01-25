144d113647638cc0977c287cb6032232ef053a29
Dishant <dishantmehra20041@gmail.com>
2024-01-25 20:47:32 +0530

    Added Navbar items and footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import Image from "next/image"
+import Link from "next/link"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            height={228}
+            width={38}
+          />
+        </Link>
+      </div>
+    </footer>
   )
 }
 
